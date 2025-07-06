0f4ae3cd91ed0c9469d780b04ded2b7931cbfa50
takakowatabe <kikotkk@gmail.com>
2025-07-06 11:41:35 +0900

    build

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,11 +1,5 @@
 import CaseStudyDetail from "@/components/CaseStudyDetail";
 
-type Page = {
-    params: {
-        slug: string;
-    };
-};
-
-export default async function Page({ params }: { params: { slug: string } }) {
-  return <CaseStudyDetail slug={params.slug} />;
+export default function Page({ params }: { params: { slug: string } }) {
+    return <CaseStudyDetail slug={params.slug} />;
 }
