be7445fe9cefb568c6f54c503adf642583f3879a
takakowatabe <kikotkk@gmail.com>
2025-07-19 17:53:29 +0900

    進捗

--- src/components/RichTextRenderer.tsx
+++ src/components/RichTextRenderer.tsx
@@ -63,7 +63,6 @@ export const RichTextRenderer = ({ html }: { html: string }) => {
                     </div>
                 );
                 }
-            
 
                 if (domNode.type === "tag" && el.name === "h2") {
                     return (
